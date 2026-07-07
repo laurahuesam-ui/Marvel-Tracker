600661d9d120b112155f7ad8855bf3d1ad81dbd1
laurahuesam-ui <laura.huesam@gmail.com>
2026-07-08 00:11:42 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'marvel-tracker-v12';
+const CACHE = 'marvel-tracker-v13';
 const FILES = ['./','./index.html','./style.css','./app.js','./manifest.json'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))).then(() => self.clients.claim()))));
